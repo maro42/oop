180ef62898011f2533171d71aefa17016279d256
rhsnc <rhsnc@DESKTOP-I8U7JKS>
2020-07-31 17:53:27 +0900

    1차수정

--- src/cal/Calculator.java
+++ src/cal/Calculator.java
@@ -1,5 +1,6 @@
 package cal;
 
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class Calculator {
@@ -10,44 +11,44 @@ public class Calculator {
 		String input = scanner.nextLine();
 		
 		String[] values = input.split(" ");
-		int answer = Integer.parseInt(values[0]);
-		
-		for(int i=1; i<values.length-1; i++) {
-			String nextNum = values[i+1];
-			if("+".equals(values[i])) {
-				answer = plus(answer,nextNum);
-			}
-			
-			if("-".equals(values[i])) {
-				answer = minus(answer,nextNum);
-			}
-			
-			if("*".equals(values[i])) {
-				answer = multiply(answer,nextNum);
-			}
-			
-			if("/".equals(values[i])) {
-				answer = division(answer,nextNum);
-			}
-		}
+		BigDecimal answer = new BigDecimal(values[0]);
+
+//		for(int i=1; i<values.length-1; i++) {
+//			String nextNum = values[i+1];
+//			if("+".equals(values[i])) {
+//				answer = plus(answer,nextNum);
+//			}
+//			
+//			if("-".equals(values[i])) {
+//				answer = minus(answer,nextNum);
+//			}
+//			
+//			if("*".equals(values[i])) {
+//				answer = multiply(answer,nextNum);
+//			}
+//			
+//			if("/".equals(values[i])) {
+//				answer = division(answer,nextNum);
+//			}
+//		}
 		
 		System.out.println(answer);
 	}
 	
-	private static int plus(int answer, String nextNum) {
-		return answer + Integer.parseInt(nextNum);
+	private static BigDecimal plus(BigDecimal answer, String nextNum) {
+		return answer.add(new BigDecimal(nextNum));
 	}
 	
-	private static int minus(int answer, String nextNum) {
-		return answer - Integer.parseInt(nextNum);
+	private static BigDecimal minus(BigDecimal answer, String nextNum) {
+		return answer.subtract(new BigDecimal(nextNum));
 	}
 	
-	private static int multiply(int answer, String nextNum) {
-		return answer * Integer.parseInt(nextNum);
+	private static BigDecimal multiply(BigDecimal answer, String nextNum) {
+		return answer.multiply(new BigDecimal(nextNum));
 	}
 	
-	private static int division(int answer, String nextNum) {
-		return answer / Integer.parseInt(nextNum);
+	private static BigDecimal division(BigDecimal answer, String nextNum) {
+		return answer.divide(new BigDecimal(nextNum));
 	}
 
 }
